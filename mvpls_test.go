8c286f15aff248d180caa140d870a6ba71ba583d
joshu <joshuapetitma@yahoo.com>
2020-12-31 13:34:04 -0500

    adds test for probing directory

--- mvpls_test.go
+++ mvpls_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"testing"
 )
 
@@ -45,23 +46,52 @@ func TestMoveFile(t *testing.T) {
 	}
 }
 
+func TestProbeDirectory(t *testing.T) {
+	testDir := createTestDir()
+	target := filepath.Join(testDir, "moved")
+	fmt.Println(testDir)
+	movedDir := filepath.Join(testDir, "moved")
+	defer os.RemoveAll(testDir)
+
+	regex, _ := regexp.Compile(".*\\.png")
+	ProbeDirectory(testDir, target, regex)
+
+	path := testDir
+	var full string
+	for i := 1; i < 4; i++ {
+		for j := 1; j < 3; j++ {
+			full = filepath.Join(movedDir, fmt.Sprintf("file%d%d.png", i, j))
+
+			if _, err := os.Stat(full); err != nil && os.IsNotExist(err) {
+				t.Error(fmt.Sprintf("Test file %s is not present", full))
+			}
+
+			full = filepath.Join(path, fmt.Sprintf("file%d%d.jpg", i, j))
+			if _, err := os.Stat(full); err != nil && os.IsNotExist(err) {
+				t.Error(fmt.Sprintf("Test file %s should not have moved", full))
+			}
+		}
+		path = filepath.Join(path, fmt.Sprintf("level%d", i+1))
+	}
+}
+
 func createTestDir() (testDir string) {
 	pdir := os.TempDir()
 	testDir, _ = ioutil.TempDir(pdir, "*-mvplsTest")
 
-	fmt.Println(testDir)
 	movedDir := filepath.Join(testDir, "moved")
 	os.Mkdir(movedDir, 0777)
 	testSubs := filepath.Join(testDir, "level2/level3/level4")
 	os.MkdirAll(testSubs, 0777)
 
-	subs := [...]string{"file11.jpg", "file11.png", "level2/file23.jpg", "level2/file21.png", "level2/file22.png", "level2/level3/file31.jpg", "level2/level3/file32.png"}
+	subs := [...]string{"file11.jpg", "file12.jpg", "file11.png", "file12.png", "level2/file21.jpg",
+		"level2/file22.jpg", "level2/file21.png", "level2/file22.png", "level2/level3/file31.jpg",
+		"level2/level3/file32.jpg", "level2/level3/file31.png", "level2/level3/file32.png"}
 	var sub string
 
 	for i := 0; i < len(subs); i++ {
 		sub = filepath.Join(testDir, subs[i])
 		os.Create(sub)
 	}
-
 	return
 }
